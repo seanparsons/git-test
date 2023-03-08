36d2876774630125f7d2d941e7c106d6cca8d151
Sean Parsons <217400+seanparsons@users.noreply.github.com>
2023-03-08 11:18:03 +0000

    First cut.

---
+++ src/playground.js
@@ -0,0 +1,59 @@
+import { View } from 'utopia-api'
+import '../public/globals.css'
+
+export var Playground = () => {
+  return (
+    <div
+      style={{
+        height: '100%',
+        width: '100%',
+        contain: 'layout',
+      }}
+      data-uid='a7b'
+    >
+      {[].length === 0 ? (
+        [].length === 0 ? (
+          <div
+            style={{
+              height: 150,
+              width: 150,
+              position: 'absolute',
+              left: 154,
+              top: 134,
+              backgroundColor: 'lightblue',
+            }}
+            data-uid='bla'
+          />
+        ) : null
+      ) : (
+        <div
+          style={{
+            height: 150,
+            position: 'absolute',
+            left: 154,
+            top: 134,
+          }}
+          data-uid='b15'
+        >
+          <img
+            src='https://github.com/concrete-utopia/utopia/blob/master/editor/resources/editor/pyramid_fullsize@2x.jpg?raw=true'
+            alt='Utopia logo'
+            style={{ height: '100%' }}
+            data-uid='b0e'
+          />
+        </div>
+      )}
+      <div
+        style={{
+          height: 150,
+          width: 150,
+          position: 'absolute',
+          left: 300,
+          top: 300,
+          backgroundColor: 'darkblue',
+        }}
+        data-uid='blc'
+      />
+    </div>
+  )
+}
---
+++ utopia/storyboard.js
@@ -0,0 +1,22 @@
+import * as React from 'react'
+import { Scene, Storyboard } from 'utopia-api'
+import { App } from '/src/app.js'
+import { Playground } from '/src/playground.js'
+
+export var storyboard = (
+  <Storyboard data-uid='0cd'>
+    <Scene
+      style={{
+        width: 700,
+        height: 759,
+        position: 'absolute',
+        left: 212,
+        top: 128,
+      }}
+      data-label='Playground'
+      data-uid='3fc'
+    >
+      <Playground style={{}} data-uid='d9d' />
+    </Scene>
+  </Storyboard>
+)
